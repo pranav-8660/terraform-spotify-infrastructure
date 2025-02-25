821cfde4f471700e9083a904c28044d092149bb6
pranav-8660 <pranavcse2003@gmail.com>
2025-02-25 12:20:20 +0530

    created a playlist on spotify where id is dynamically fetched by spotify-search-track

--- playlist.tf
+++ playlist.tf
@@ -2,3 +2,13 @@ resource "spotify_playlist" "bollywood" {
   name = "My Fav Playlist"
   tracks = ["131yybV7A3TmC34a0qE8u8"]
 }
+
+data "spotify_search_track" "searchdata" {
+  name = "Eminem"
+}
+
+resource "spotify_playlist" "name" {
+  name = "Slim Shady"
+  tracks = [data.spotify_search_track.searchdata.tracks[0].id,
+  data.spotify_search_track.searchdata.tracks[1].id,data.spotify_search_track.searchdata.tracks[2].id]
+}
